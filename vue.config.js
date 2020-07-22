a5a579105be0257eef236b43ffea2f33a1cdc293
iantairea <waipuna@gmail.com>
2020-07-22 21:38:02 +1200

    fixed path in config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // vue.config.js
 module.exports = {
   // options...
-  publicPath: "/timetable/",
+  publicPath: "/timetable/",  //because its hosted at tutoa.co.nz/timetable
   devServer: {
     proxy: 'http://backend.test/',
   }
